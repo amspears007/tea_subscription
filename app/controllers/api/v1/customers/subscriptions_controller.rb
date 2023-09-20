e11cb82cc5b5cb63f68397ea02071c2e71bf1599
Amy Spears <amspears007@gmail.com>
2023-09-20 11:08:10 -0600

    Add sad path testing for post request

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -1,13 +1,11 @@
 class Api::V1::Customers::SubscriptionsController < ApplicationController
   def create
-    # customer = Customer.find(params[:customer_id])
-    new_sub = Subscription.create!(subscription_params)
+    new_sub = Subscription.new(subscription_params)
     if new_sub.save
 			render json: SubscriptionsSerializer.new(new_sub), status: 201
 		else
-			render json: { error: sub.errors.full_messages.to_sentence }, status: 400
+			render json: { error: new_sub.errors.full_messages.to_sentence }, status: 400
 		end
-    # require 'pry'; binding.pry
   end
 end
 
--- spec/requests/api/v1/subscriptions/new_spec.rb
+++ spec/requests/api/v1/subscriptions/new_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe "Create a new subscription" do
     @customer1 = Customer.create!(first_name: 'Amy', last_name: 'Spears', email: 'amers123@gmail.com', address: '123 street, denver, CO, 80028')
     @tea1 = Tea.create!(title: 'Earl Grey', temperature: 'boiling', brew_time: '4-6 minutes')
     @sub1 = Subscription.create!(title: "Super Fun Tea", price: 11.99, status: "inactive", frequency: "bi-weekly", customer_id: @customer1.id, tea_id: @tea1.id)
-
   end
+
   describe "happy paths" do
     it "can create a subscription with a customer and tea id" do
 
@@ -24,7 +24,6 @@ RSpec.describe "Create a new subscription" do
         expect(response).to be_successful
         expect(response.status).to eq(201)
         subscription = JSON.parse(response.body, symbolize_names: true)
-# require 'pry'; binding.pry
         expect(subscription).to be_a(Hash)
         expect(subscription[:data]).to have_key(:type)
         expect(subscription[:data]).to have_key(:id)
@@ -37,4 +36,28 @@ RSpec.describe "Create a new subscription" do
         expect(subscription[:data][:attributes][:price]).to be_a(Float)
       end
     end
+
+    describe "sad paths" do
+      it "will return an error if a params is blank" do
+
+        bad_params = {
+          title: 'Tasty Teas R Us',
+          price: 10.00,
+          status: 'active',
+          frequency: 'weekly',
+          customer_id: @customer1.id,
+          tea_id: nil
+          }
+
+          post "/api/v1/customers/#{@customer1.id}/subscriptions", params: bad_params
+
+          expect(response).to_not be_successful
+          expect(response.status).to eq(400)
+          bad_sub = JSON.parse(response.body, symbolize_names: true)
+          expect(bad_sub).to be_a(Hash)
+          expect(bad_sub).to have_key(:error)
+          expect(bad_sub[:error]).to be_a(String)
+          expect(bad_sub[:error]).to eq("Tea must exist")
+        end
+      end
   end
