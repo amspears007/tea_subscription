db337c015e2c0185a6393c13954cb5e80d827bef
Amy Spears <amspears007@gmail.com>
2023-09-20 21:26:00 -0600

    Add test and data for patch request

--- spec/requests/api/v1/subscriptions/index_spec.rb
+++ spec/requests/api/v1/subscriptions/index_spec.rb
@@ -9,10 +9,9 @@ RSpec.describe "it returns and updates subscriptions" do
     @sub2 = Subscription.create!(title: "Monthly Tea", price: 10.00, status: "active", frequency: "monthly", customer_id: @customer1.id, tea_id: @tea2.id)
     @sub3 = Subscription.create!(title: "Tea, Tea, Tea", price: 12.50, status: "active", frequency: "weekly", customer_id: @customer1.id, tea_id: @tea1.id)
   end
-  describe "happy paths" do
+  describe "happy paths for getting all subscriptions" do
     it "returns all valid subscriptions" do
       get "/api/v1/customers/#{@customer1.id}/subscriptions"
-      # require 'pry'; binding.pry
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
@@ -36,7 +35,7 @@ RSpec.describe "it returns and updates subscriptions" do
     end
   end
 
-  describe "sad paths" do
+  describe "sad paths for getting all subscriptions" do
     it "returns an error message" do
       get "/api/v1/customers/99/subscriptions"
 
@@ -46,6 +45,26 @@ RSpec.describe "it returns and updates subscriptions" do
       expect(bad_subs).to be_a(Hash)
       expect(bad_subs).to have_key(:error)
       expect(bad_subs[:error]).to eq('Customer must exist')
+    end
+  end
+
+  describe "an endpoint to cancel a customers subscription" do
+    it "changes status to inactive" do
+      patch "/api/v1/customers/#{@customer1.id}/subscriptions/#{@sub3.id}"
+
+      subscription_params = {
+        title: "Tea, Tea, Tea",
+        price: 12.50,
+        status: "inactive",
+        frequency: "weekly",
+        customer_id: @customer1.id,
+        tea_id: @tea1.id
+        }
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+      subscription = JSON.parse(response.body, symbolize_names: true)
+      expect(subscription).to be_a(Hash)
     end
   end
 end
