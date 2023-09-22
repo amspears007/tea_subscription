848201c7fcceb838866e2f84d2e65e0e5d019485
Amy Spears <amspears007@gmail.com>
2023-09-21 20:24:52 -0600

    Add validations and enums to subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates_presence_of :title, :status, :frequency, :price
+
+  enum status: { active: 0, inactive: 1 }
 end
